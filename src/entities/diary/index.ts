bc979461464cc7fd7b8bcc75653326271dc93592
nakyeong <yunna3425@naver.com>
2026-05-06 13:41:43 +0900

    chore: 삭제된 DiaryEmptyState export 제거 (#19)

--- src/entities/diary/index.ts
+++ src/entities/diary/index.ts
@@ -1,6 +1,5 @@
 export type { Diary, EmotionIntensity, TodayDiary } from "./model/diary.types";
 export { DiaryCard } from "./ui/DiaryCard";
 export { DiaryChip } from "./ui/DiaryChip";
-export { DiaryEmptyState } from "./ui/DiaryEmptyState";
 export { DiaryListItem } from "./ui/DiaryListItem";
 export { DiaryListSection } from "./ui/DiaryListSection";
