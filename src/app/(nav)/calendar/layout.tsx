2849b1dc2015ac259e9767f5c3bfb962276937b6
nakyeong <yunna3425@naver.com>
2026-05-21 17:22:01 +0900

    refactor: 캘린더 페이지를 레이아웃 및 메인 뷰로 분리 (#65)

---
+++ src/app/(nav)/calendar/layout.tsx
@@ -0,0 +1,19 @@
+import type { ReactNode } from "react";
+import { CalendarHeader } from "@/widgets/calendar";
+
+type CalendarLayoutProps = {
+  children: ReactNode;
+};
+
+const CalendarLayout = ({ children }: CalendarLayoutProps): React.ReactElement => {
+  return (
+    <div className="flex flex-col h-full bg-gray-0">
+      <CalendarHeader />
+      <div className="flex-1 overflow-y-auto pb-24 [&::-webkit-scrollbar]:hidden [-ms-overflow-style:none] [scrollbar-width:none]">
+        {children}
+      </div>
+    </div>
+  );
+};
+
+export default CalendarLayout;
