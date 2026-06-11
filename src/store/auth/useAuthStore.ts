86308679ca0434019f4851286a9323b9f3b9b361
cindy-chaewon <chaewon1769@daum.net>
2026-06-11 23:05:29 +0900

    fix: useAuthStore 하드코딩 액세스 토큰 제거 (#71)

--- src/store/auth/useAuthStore.ts
+++ src/store/auth/useAuthStore.ts
@@ -10,8 +10,7 @@ type AuthState = {
 export const useAuthStore = create<AuthState>()(
   devtools(
     (set) => ({
-      accessToken:
-        "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiI5MDAyIiwiZXhwIjoxNzgxMDExODE2LCJ0eXBlIjoiYWNjZXNzIiwiaWF0IjoxNzgxMDEwOTE2LCJqdGkiOiI3MDA4NDhiMS1kMjQ2LTQ0MjktOGUxNS01NDQyZDg2NDdmZGEifQ.IAvqHUWV5dBDKWly6afcoTkQwCnGXoHdp7IJGZLExuM",
+      accessToken: null,
       setAuth: (accessToken: string) => {
         set({ accessToken }, false, "auth/setAuth");
       },
