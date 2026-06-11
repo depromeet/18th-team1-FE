b9772ffd3395434cdd201b2d3c337fa46ba7a22d
nakyeong <yunna3425@naver.com>
2026-06-12 04:09:17 +0900

    fix: IcShare viewBox 24x24 정규화 (#72)

--- src/shared/ui/icons/IcShare.tsx
+++ src/shared/ui/icons/IcShare.tsx
@@ -2,25 +2,25 @@ import { BaseIcon } from "./BaseIcon";
 import type { IconProps } from "./icon.types";
 
 export const IcShare = (props: IconProps) => (
-  <BaseIcon viewBox="0 0 34 34" fill="none" {...props}>
+  <BaseIcon viewBox="0 0 24 24" fill="none" {...props}>
     <path
-      d="M14.1379 13.172L16.9975 10.3125L19.857 13.172"
+      d="M9.9797 9.2979L11.9982 7.2794L14.0167 9.2979"
       stroke="currentColor"
-      strokeWidth="1.55244"
+      strokeWidth="1.0958"
       strokeLinecap="round"
       strokeLinejoin="round"
     />
     <path
-      d="M16.9976 10.6675L16.9976 19.1513"
+      d="M11.9983 7.53L11.9983 13.5186"
       stroke="currentColor"
-      strokeWidth="1.55244"
+      strokeWidth="1.0958"
       strokeLinecap="round"
       strokeLinejoin="round"
     />
     <path
-      d="M23.375 15.811V21.9127C23.375 22.8926 22.5807 23.6869 21.6008 23.6869H12.3992C11.4193 23.6869 10.625 22.8926 10.625 21.9127V15.811"
+      d="M16.5 11.1607V15.4678C16.5 16.1595 15.9393 16.7143 15.2476 16.7143H8.7524C8.0607 16.7143 7.5 16.1595 7.5 15.4678V11.1607"
       stroke="currentColor"
-      strokeWidth="1.55244"
+      strokeWidth="1.0958"
       strokeLinecap="round"
       strokeLinejoin="round"
     />
