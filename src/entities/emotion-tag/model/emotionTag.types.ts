cb66ff8d5c9fe4751e5bc09d00b2257e258e30c3
cindy-chaewon <chaewon1769@daum.net>
2026-06-16 00:16:50 +0900

    feat: NeedTag 감정 태그 API 추가 (#89)

--- src/entities/emotion-tag/model/emotionTag.types.ts
+++ src/entities/emotion-tag/model/emotionTag.types.ts
@@ -11,10 +11,20 @@ export interface EmotionTag {
   emotionRangeId: number;
 }
 
+export interface NeedTag {
+  id: number;
+  label: string;
+  type: "NEED";
+}
+
 export interface EmotionTagsResponse {
   tags: EmotionTag[];
 }
 
 export interface ToneTagsResponse {
   tags: ToneTag[];
+}
+
+export interface NeedTagsResponse {
+  tags: NeedTag[];
 }
